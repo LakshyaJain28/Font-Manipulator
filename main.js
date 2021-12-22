afeb9b458d75ed2fab1c02ec5cc75efeddad19ee
LakshyaJain28 <79844523+LakshyaJain28@users.noreply.github.com>
2021-12-22 16:42:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,5 +25,18 @@ function gotPoses(results)
 {
     if(results.length>0){
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
     }
+}
+function draw()
+{
+    background("grey");
+fill(0,0,255);
+stroke(0,0,255);
+textSize(difference);
+text("Peter",noseX,noseY);
 }
